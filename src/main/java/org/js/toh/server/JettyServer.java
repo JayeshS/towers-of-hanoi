7e3442370780b5c05a3a4932cc5cdfb48596c989
JayeshS <jayesh.shere@gmail.com>
2013-07-09 15:33:27 +1000

    Now uses PORT env variable

--- src/main/java/org/js/toh/server/JettyServer.java
+++ src/main/java/org/js/toh/server/JettyServer.java
@@ -5,12 +5,21 @@ import org.eclipse.jetty.server.handler.ResourceHandler;
 
 public class JettyServer {
 
+    private static final String PORT_KEY = "PORT";
+
     public static void main(String[] args) throws Exception {
         checkArgs(args);
-        Server server = new Server(8080);
+        Server server = new Server(getPort());
         server.setHandler(createResourceHandler(args[0]));
         server.start();
         server.join();
+    }
+
+    private static int getPort() {
+        if (System.getenv().containsKey(PORT_KEY)) {
+            return Integer.valueOf(System.getenv(PORT_KEY));
+        }
+        return 8080;
     }
 
     private static void checkArgs(String[] args) {
